93a94b893f62f0385f2dee51418069c0db89173b
Bozhena Shmega <bozhenabee@gmail.com>
2018-10-14 20:39:00 +0300

    css in main.js was corrected

--- main.js
+++ main.js
@@ -3,22 +3,14 @@ new WOW().init();
 $(document).ready(function() {
     $(".pricing__card").each(function(){
         $(this).mouseout(function() {
-            $(this).find(".pricing__card__top")
-                .css("background-color", "#323232");
-            $(this).find(".pricing__button")
-                .css("background-color", "#ffffff")
-                .css("border-color", "#323232")
-                .css("color", "#323232");
+            $(this).find(".pricing__card__top").css({backgroundColor: "#323232"});
+            $(this).find(".pricing__button").css({backgroundColor: "#ffffff", borderColor: "#323232", color: "#323232"});
             $(this).find(".price").addClass("blue-text");
         });
 
         $(this).mousemove(function() {
-            $(this).find(".pricing__card__top")
-                .css("background-color", "#0199e6");
-            $(this).find(".pricing__button")
-                .css("background-color", "#0199e6")
-                .css("border-color", "#0199e6")
-                .css("color", "#ffffff");
+            $(this).find(".pricing__card__top").css({backgroundColor: "#0199e6"});
+            $(this).find(".pricing__button").css({backgroundColor: "#0199e6", borderColor: "#0199e6", color: "#ffffff"});
             $(this).find(".price").removeClass("blue-text");
         });
     })
