97c5d222dd72711cc5d49746b4666709567f323e
Bozhena Shmega <bozhenabee@gmail.com>
2018-10-14 19:04:03 +0300

    js syntax was corrected

--- main.js
+++ main.js
@@ -2,19 +2,23 @@ new WOW().init();
 
 $(document).ready(function() {
     $(".pricing__card").each(function(){
-        $(this).mouseout(function(e) {
-            $(this).find(".pricing__card__top").css("background-color", "#323232");
-            $(this).find(".pricing__button").css("background-color", "#ffffff");
-            $(this).find(".pricing__button").css("border-color", "#323232");
-            $(this).find(".pricing__button").css("color", "#323232");
+        $(this).mouseout(function() {
+            $(this).find(".pricing__card__top")
+                .css("background-color", "#323232");
+            $(this).find(".pricing__button")
+                .css("background-color", "#ffffff")
+                .css("border-color", "#323232")
+                .css("color", "#323232");
             $(this).find(".price").addClass("blue-text");
         });
 
-        $(this).mousemove(function(e) {
-            $(this).find(".pricing__card__top").css("background-color", "#0199e6");
-            $(this).find(".pricing__button").css("background-color", "#0199e6");
-            $(this).find(".pricing__button").css("border-color", "#0199e6");
-            $(this).find(".pricing__button").css("color", "#ffffff");
+        $(this).mousemove(function() {
+            $(this).find(".pricing__card__top")
+                .css("background-color", "#0199e6");
+            $(this).find(".pricing__button")
+                .css("background-color", "#0199e6")
+                .css("border-color", "#0199e6")
+                .css("color", "#ffffff");
             $(this).find(".price").removeClass("blue-text");
         });
     })
