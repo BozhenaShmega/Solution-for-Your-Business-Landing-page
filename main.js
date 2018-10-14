829e28cfd0f9b64e2ad8f7280ed318033fe09df9
Bozhena Shmega <bozhenabee@gmail.com>
2018-10-15 02:16:23 +0300

    hover method was added

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 new WOW().init();
 
 $(document).ready(function() {
-    $(".pricing__card").each(function(){
-        $(this).mouseout(function() {
-            $(this).find(".pricing__card__top").css({backgroundColor: "#323232"});
-            $(this).find(".pricing__button").css({backgroundColor: "#ffffff", borderColor: "#323232", color: "#323232"});
-            $(this).find(".price").addClass("blue-text");
-        });
 
-        $(this).mousemove(function() {
-            $(this).find(".pricing__card__top").css({backgroundColor: "#0199e6"});
-            $(this).find(".pricing__button").css({backgroundColor: "#0199e6", borderColor: "#0199e6", color: "#ffffff"});
+    $(".pricing__card").each(function(){
+        $(this).hover(function() {
+            $(this).find(".pricing__card__top").css({
+                backgroundColor: "#0199e6"
+            });
+            $(this).find(".pricing__button").css({
+                backgroundColor: "#0199e6", 
+                borderColor: "#0199e6", 
+                color: "#ffffff"
+            });
             $(this).find(".price").removeClass("blue-text");
-        });
+        }, 
+        
+        function() {
+            $(this).find(".pricing__card__top").css({
+                backgroundColor: "#323232"
+            });
+            $(this).find(".pricing__button").css({
+                backgroundColor: "#ffffff", 
+                borderColor: "#323232", 
+                color: "#323232"
+            });
+            $(this).find(".price").addClass("blue-text");
+        })
     })
     
     $('.nav-link').on('click', function(){
